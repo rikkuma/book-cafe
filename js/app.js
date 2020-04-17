6fad238496e3737a878a9f133114720da89d927d
github user name <rikkuma.2.25@gmail.com>
2020-04-17 17:40:52 +0900

    スムーズスクロールを実装

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(function(){
+
     //メニューバー背景切り替え
     var targetHeight = $('.js-float-menu-target').height() - 40;
     $(window).on('scroll', function(){
@@ -7,28 +8,23 @@ $(function(){
 
     //メニューが左右からフェードイン
     $(window).on('scroll',function(){
-        
-            //スクロール量を取得
-            var scrollTop = $(window).scrollTop();
-
-            //eachを利用して「menues」クラスの要素それぞれに処理を行う
-            $('.menues').each(function(){
-
-                //ページ最上部から「menues」のついた要素までの距離を取得
-                var offset_top = $(this).offset().top;
-                var windowHeight = $(window).height();
-
-                //スクロール量と要素の一からマージンを引いた値を比較
-                if(scrollTop > offset_top - windowHeight){
-
-                    //スクロール量が所定の位置を超えた時にfadeinクラスを付与
-                    $(this).addClass('js-fadein');
-                }else{
-                    //スクロール量が所定の位置を超えていない場合はクラスを外す
-                    $(this).removeClass('js-fadein');
+        //スクロール量を取得
+        var scrollTop = $(window).scrollTop();  
+        //eachを利用して「menues」クラスの要素それぞれに処理を行う
+        $('.menues').each(function(){
+            //ページ最上部から「menues」のついた要素までの距離を取得
+            var offset_top = $(this).offset().top;
+            var windowHeight = $(window).height();
+            //スクロール量と要素の一からマージンを引いた値を比較
+            if(scrollTop > offset_top - windowHeight){
+                //スクロール量が所定の位置を超えた時にfadeinクラスを付与
+                $(this).addClass('js-fadein');
+            }else{
+                //スクロール量が所定の位置を超えていない場合はクラスを外す
+                $(this).removeClass('js-fadein');
                 }
             });  
-        });
+    });
 
 
     //スマートフォン時のメニュー開閉
@@ -38,7 +34,22 @@ $(function(){
     });
 
     $('.nav-link').on('click',function(){
+        $('.js-toggle-sp-menu').removeClass('active');
         $('.js-nav').removeClass('active');
     });
 
+    //スムーズスクロール
+    $('a[href^="#"]').click(function() {
+        // スクロールの速度
+        var speed = 400; // ミリ秒
+        // アンカーの値取得
+        var href= $(this).attr("href");
+        // 移動先を取得
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        // 移動先を数値で取得
+        var position = target.offset().top;
+        // スムーススクロール
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+     });
 });
